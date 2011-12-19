f09e625841b82a6b169e7a49e18ea11bdd053f4a
Evan Driscoll <driscoll@cs.wisc.edu>
2011-12-19 17:46:08 -0600

    Linux readdir workingish I guess

---
+++ src/bare/generic.py
@@ -0,0 +1,28 @@
+
+class DirectoryEntry(object):
+    def __init__(self, name, type, inode, **kwargs):
+        self.__name = name
+        self.__type = type
+        self.__inode = inode
+        self.__attrs = kwargs
+
+    def __str__(self):
+        return '<%s, a %s @ %s>' % (self.__name, self.__type, str(self.__inode))
+
+
+class FileType(object):
+    def __init__(self, desc):
+        self.__desc = desc
+
+    def __str__(self):
+        return self.__desc
+
+RegularFile  = FileType("regular file")
+Directory    = FileType("directory")
+SymbolicLink = FileType("symbolic link")
+BlockDevice  = FileType("block device")
+CharacterDevice = FileType("character device")
+NamedPipe    = FileType("named pipe")
+Socket       = FileType("socket")
+Whiteout     = FileType("whiteout")
+UnknownType  = FileType("unknown")
