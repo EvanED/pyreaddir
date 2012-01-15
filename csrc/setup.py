c28c0c902785c935ea364bc6d13e332acd523238
Evan Driscoll <driscoll@cs.wisc.edu>
2012-01-15 00:32:05 -0600

    Added directory and skeleton script for C wrapper

---
+++ csrc/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('readdir_c',
+                    sources = ['readdir.c'])
+
+setup (name = 'PyReaddir_c',
+       version = '0.0.1',
+       description = 'C-to-Python bridge for readdir()',
+       ext_modules = [module1])
+
