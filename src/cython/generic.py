d2ef373d028b17d3f31811b8f1d8679b26900bd2
Evan Driscoll <driscoll@cs.wisc.edu>
2012-01-31 10:50:35 -0600

    "cp ../bare/generic.py ." so that i can start working on it for the Cython version

---
+++ src/cython/generic.py
@@ -0,0 +1,40 @@
+import os.path
+
+
+class DirectoryEntry(object):
+    def __init__(self, name, path, type, inode, **kwargs):
+        self.__path = path
+        self.__name = name
+        self.__type = type
+        self.__inode = inode
+        self.__attrs = kwargs
+
+    def __str__(self):
+        return '<%s, a %s>' % (os.path.join(self.__path, self.__name),
+                                    self.__type)
+    def __repr__(self):
+        return self.__str__()
+
+    def is_directory(self):
+        return self.__type == Directory
+
+    def name(self):
+        return self.__name
+
+
+class FileType(object):
+    def __init__(self, desc):
+        self.__desc = desc
+
+    def __str__(self):
+        return self.__desc
+
+RegularFile  = FileType("regular file")
+Directory    = FileType("directory")
+SymbolicLink = FileType("symbolic link")
+BlockDevice  = FileType("block device")
+CharacterDevice = FileType("character device")
+NamedPipe    = FileType("named pipe")
+Socket       = FileType("socket")
+Whiteout     = FileType("whiteout")
+UnknownType  = FileType("unknown")
