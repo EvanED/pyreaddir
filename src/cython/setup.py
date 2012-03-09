2b2823a749b3bde3263be18a13da4791577b2810
Evan Driscoll <driscoll@cs.wisc.edu>
2012-03-09 10:43:56 -0600

    Tell disttools about the pure python modules too

--- src/cython/setup.py
+++ src/cython/setup.py
@@ -3,6 +3,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("readdir_bare_posix", ["lib/posix.pyx"])]
+    cmdclass    = {'build_ext': build_ext},
+    ext_modules = [Extension("readdir_bare_posix", ["lib/posix.pyx"])],
+    package_dir = {'': 'lib'},
+    py_modules  = ['generic', 'posix2']
 )
