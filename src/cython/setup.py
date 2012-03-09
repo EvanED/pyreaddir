e6d07d5eddc94fac22d5eea7fca4a5a47783127c
Evan Driscoll <driscoll@cs.wisc.edu>
2012-03-09 11:09:31 -0600

    'python setup.py test' now runs the unit tests and prints results

--- src/cython/setup.py
+++ src/cython/setup.py
@@ -3,7 +3,6 @@ from distutils.extension import Extension
 from distutils.cmd import Command
 from Cython.Distutils import build_ext
 
-
 class TestCommand(Command):
     def initialize_options(self):
         pass
@@ -12,7 +11,36 @@ class TestCommand(Command):
         pass
 
     def run(self):
-        print "Hello world!"
+        import unittest
+        import sys
+        t1r = unittest.main(module='tests', exit=False, argv=[sys.argv[0]])
+        t2r = unittest.main(module='test_directoryentry', exit=False, argv=[sys.argv[0]])
+        total = t1r.result.testsRun + t2r.result.testsRun
+        xfail_fail = len(t1r.result.expectedFailures
+                         + t2r.result.expectedFailures)
+        xfail_success = len(t1r.result.unexpectedSuccesses
+                            + t2r.result.unexpectedSuccesses)
+        skipped = len(t1r.result.skipped + t2r.result.skipped)
+        failures = len(t1r.result.failures + t2r.result.failures)
+        errors = len(t1r.result.errors + t2r.result.errors)
+        success = total - xfail_fail - xfail_success - skipped - failures - errors
+        print "********************"
+        print "  Overall results:"
+        print ""
+        print "  successes             ", success
+        print "  skipped               ", skipped
+        print "  expected failures     ", xfail_fail
+        print "  total pass            ", (success + skipped + xfail_fail)
+        print ""
+        print "  errors                ", errors
+        print "  failures              ", failures
+        print "  unexpected successes  ", xfail_success
+        print "  total fail            ", (errors + failures + xfail_success)
+        print ""
+        print "  total                 ", total
+
+        if xfail_success + failures + errors > 0:
+            sys.exit(1)
 
     sub_commands = []
     user_options = []
