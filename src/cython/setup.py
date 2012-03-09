69a4f5b1acad9dfc7ba6b2963b682ec8731e16ef
Evan Driscoll <driscoll@cs.wisc.edu>
2012-03-09 10:53:15 -0600

    Example new command

--- src/cython/setup.py
+++ src/cython/setup.py
@@ -1,9 +1,26 @@
 from distutils.core import setup
 from distutils.extension import Extension
+from distutils.cmd import Command
 from Cython.Distutils import build_ext
 
+
+class TestCommand(Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print "Hello world!"
+
+    sub_commands = []
+    user_options = []
+
+##############################################
 setup(
-    cmdclass    = {'build_ext': build_ext},
+    cmdclass    = {'build_ext': build_ext,
+                   'test'     : TestCommand},
     ext_modules = [Extension("readdir_bare_posix", ["lib/posix.pyx"])],
     package_dir = {'': 'lib'},
     py_modules  = ['generic', 'posix2']
